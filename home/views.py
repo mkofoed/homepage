a2fadec38d07667a0bd18067fee0108adcbfafef
Mikkel Kofoed <mikkel.kofoed@gmail.com>
2023-03-07 20:22:41 +0100

    WhiteNoise

--- home/views.py
+++ home/views.py
@@ -6,5 +6,5 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs: object) -> dict:
         context = super().get_context_data(**kwargs)
-        context["title"] = "asd"
+        context["title"] = "assd"
         return context

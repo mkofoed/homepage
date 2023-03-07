9bc9eff989f82f2b8fa449536f57beee30ee7728
Mikkel Kofoed <mikkel.kofoed@gmail.com>
2023-03-07 17:36:16 +0100

    WhiteNoise

--- home/views.py
+++ home/views.py
@@ -6,5 +6,5 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs: object) -> dict:
         context = super().get_context_data(**kwargs)
-        context["title"] = "Home"
+        context["title"] = "asd"
         return context
--- project/settings.py
+++ project/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -145,10 +146,13 @@ STATIC_URL = "/static/"
 # Define the directories where Django should look for static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "project/static/"),
 ]
 
 # Define the directory where Django should collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

a66260bfc0d720d5f026f66e6a2eca1bca1ff0fc
Mikkel Kofoed <mikkel.kofoed@fellowmind.dk>
2025-12-05 22:01:18 +0100

    Update

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,10 @@
+import sys
+import django
 from django.conf import settings
 
 def site_name(request):
-    return {'SITE_NAME': settings.SITE_NAME}
+    return {
+        'SITE_NAME': settings.SITE_NAME,
+        'PYTHON_VERSION': f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}",
+        'DJANGO_VERSION': django.get_version(),
+    }
